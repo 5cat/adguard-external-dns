f4e6f3efa4044a711ca0896ba5c78cee17a14433
5cat <cat@heaven.ist>
2023-03-01 23:17:07 +0800

    adding regex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::{
     collections::HashMap,
     hash::{Hash, Hasher},
@@ -5,7 +6,7 @@ use std::{
 
 use clap::Parser;
 
-use futures::TryStreamExt;
+use futures::{pin_mut, StreamExt, TryStreamExt};
 use k8s_openapi::{api::networking::v1::Ingress, Metadata};
 use kube::{
     api::{Api, ListParams, Patch, PatchParams},
@@ -100,17 +101,16 @@ impl<'a> AdGuard<'a> {
     }
 }
 
-
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct IngressRecord {
     host: String,
-    ip: String
+    ip: String,
 }
 
 #[derive(Debug, Clone)]
 struct IngressNeededInfo {
     current: IngressRecord,
-    old: Option<IngressRecord>
+    old: Option<IngressRecord>,
 }
 
 fn extract_needed_info(ing: &Ingress) -> Vec<IngressNeededInfo> {
@@ -151,14 +151,20 @@ fn extract_needed_info(ing: &Ingress) -> Vec<IngressNeededInfo> {
     assert!(old_ips.len() <= 1);
     let mut old_record: Option<IngressRecord> = None;
     if old_hosts.len() > 0 {
-        old_record = Some(IngressRecord {host: old_hosts.get(0).unwrap().clone(), ip: old_ips.get(0).unwrap().clone()});
+        old_record = Some(IngressRecord {
+            host: old_hosts.get(0).unwrap().clone(),
+            ip: old_ips.get(0).unwrap().clone(),
+        });
     }
     if let Some(ip_clear) = ip {
         return hosts
             .into_iter()
             .map(|host| IngressNeededInfo {
-                current: IngressRecord { host: host.clone(), ip: ip_clear.clone() },
-                old: old_record.clone()
+                current: IngressRecord {
+                    host: host.clone(),
+                    ip: ip_clear.clone(),
+                },
+                old: old_record.clone(),
             })
             .collect();
     }
@@ -193,12 +199,15 @@ struct MyOptions {
     adguard_host: String,
     #[arg(short, long, env, default_value_t = false)]
     adguard_use_https: bool,
+    #[arg(env, short, long)]
+    domain_regex: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = MyOptions::parse();
     dbg!(&opts);
+
     let adguard = AdGuard::new(&opts.adguard_host, opts.adguard_use_https);
     // Infer the runtime environment and try to create a Kubernetes Client
     let client = Client::try_default().await?;
@@ -206,60 +215,84 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Read pods in the configured namespace into the typed interface from k8s-openapi
     let ingress: Api<Ingress> = Api::all(client.clone());
 
-    watcher(ingress, ListParams::default())
-        .try_for_each(|ing| async move {
-            let client_2 = Client::try_default().await.unwrap();
-            println!("{:#?}", &ing);
-            match &ing {
-                Event::Applied(s) => {
-                    println!("Added");
-                    let ingress_namespaced: Api<Ingress> =
-                        Api::namespaced(client_2, s.metadata.namespace.as_ref().unwrap());
-                    for ini in extract_needed_info(s) {
-                        dbg!(&ini);
-                        let record = Record::new(ini.current.host.clone(), ini.current.ip.clone());
-                        if let Some(old_record) = ini.old {
-                            if old_record != ini.current {
-                                println!("deleting old record {:#?}", old_record);
-                                adguard.delete_record(&Record::new(old_record.host.clone(), old_record.ip.clone())).await.unwrap();
-                                println!("records do not match {:#?} != {:#?}", old_record, ini.current);
-                                adguard.add_record(&record).await.unwrap();
-                            }
-                        } else {
+    let domain_regex_string = &opts.domain_regex.unwrap_or(".*".to_owned()).clone();
+    let domain_regex = Regex::new(domain_regex_string).unwrap();
+    let w = watcher(ingress, ListParams::default());
+    pin_mut!(w);
+    while let Some(ing) = w.try_next().await? {
+        // .try_for_each(|ing| async {
+        let client_2 = Client::try_default().await.unwrap();
+        println!("{:#?}", &ing);
+        match &ing {
+            Event::Applied(s) => {
+                println!("Added");
+                let ingress_namespaced: Api<Ingress> =
+                    Api::namespaced(client_2, s.metadata.namespace.as_ref().unwrap());
+                for ini in extract_needed_info(s) {
+                    if !domain_regex.is_match(&ini.current.host) {
+                        continue;
+                    }
+                    dbg!(&ini);
+                    let record = Record::new(ini.current.host.clone(), ini.current.ip.clone());
+                    if let Some(old_record) = ini.old {
+                        if old_record != ini.current {
+                            println!("deleting old record {:#?}", old_record);
+                            adguard
+                                .delete_record(&Record::new(
+                                    old_record.host.clone(),
+                                    old_record.ip.clone(),
+                                ))
+                                .await
+                                .unwrap();
+                            println!(
+                                "records do not match {:#?} != {:#?}",
+                                old_record, ini.current
+                            );
                             adguard.add_record(&record).await.unwrap();
                         }
-                        update_annotations(&ingress_namespaced, &s, &ini.current)
-                            .await
-                            .unwrap();
+                    } else {
+                        adguard.add_record(&record).await.unwrap();
                     }
+                    update_annotations(&ingress_namespaced, &s, &ini.current)
+                        .await
+                        .unwrap();
                 }
-                Event::Deleted(s) => {
-                    println!("Deleted");
+            }
+            Event::Deleted(s) => {
+                println!("Deleted");
+                for ini in extract_needed_info(s) {
+                    if !domain_regex.is_match(&ini.current.host) {
+                        continue;
+                    }
+                    adguard
+                        .delete_record(&Record::new(ini.current.host, ini.current.ip))
+                        .await
+                        .unwrap();
+                }
+            }
+            Event::Restarted(ss) => {
+                println!("Restarted");
+                let current_records = adguard.get_records().await.unwrap();
+                for s in ss {
                     for ini in extract_needed_info(s) {
-                        adguard
-                            .delete_record(&Record::new(ini.current.host, ini.current.ip))
-                            .await
-                            .unwrap();
-                    }
-                }
-                Event::Restarted(ss) => {
-                    println!("Restarted");
-                    let current_records = adguard.get_records().await.unwrap();
-                    for s in ss {
-                        for ini in extract_needed_info(s) {
-                            let record = &Record::new(ini.current.host.clone(), ini.current.ip.clone());
-                            if !current_records.contains_key(&ini.current.host) {
-                                adguard.add_record(record).await.unwrap();
-                            } else if current_records.get(&ini.current.host).unwrap().eq(&ini.current.ip) {
-                                adguard.delete_record(record).await.unwrap();
-                                adguard.add_record(record).await.unwrap();
-                            }
+                        if !domain_regex.is_match(&ini.current.host) {
+                            continue;
+                        }
+                        let record = &Record::new(ini.current.host.clone(), ini.current.ip.clone());
+                        if !current_records.contains_key(&ini.current.host) {
+                            adguard.add_record(record).await.unwrap();
+                        } else if current_records
+                            .get(&ini.current.host)
+                            .unwrap()
+                            .eq(&ini.current.ip)
+                        {
+                            adguard.delete_record(record).await.unwrap();
+                            adguard.add_record(record).await.unwrap();
                         }
                     }
                 }
-            };
-            Ok(())
-        })
-        .await?;
+            }
+        };
+    }
     Ok(())
 }
